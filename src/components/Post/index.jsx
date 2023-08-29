5af7d9f41da526f6f54f46eaa71203f1b7a5cb30
Silas Duarte <silas.duarte.h@gmail.com>
2023-08-29 07:56:13 -0600

    Add header

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <article className="article">
+      <div className='container'>
+        <div className='post-wrapper'>
+          <h1>Post</h1>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Post
